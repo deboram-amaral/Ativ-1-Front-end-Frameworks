db33e25075badd0ead794b51ad8606e9d08ef5af
deboram.amaral <deboramenezes.amaral@gmail.com>
2023-08-27 15:35:11 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import rindo from './imagens/rindo.jpg';
+import alegre from './imagens/alegre.jpg';
+import bailarina from './imagens/bailarina.jpg';
+import coracao from './imagens/coracao.jpg';
 
-export default App;
+function App(){
+    return(
+      <div className="App">
+        <h1>Hello, world!</h1>
+        <img src={rindo} alt='Sorrindo' title='Sorrindo' height='200px' hidth='200px'/>
+        <p>Clique no emoji para exibir o nome dele!</p>
+        <img src={alegre} alt='Alegre' title='Alegre' height='50px' hidth='50px' hspace='10'/>
+        <img src={bailarina} alt='Bailarina' title='Bailarina' height='50px' hidth='50px' hspace='10'/>
+        <img src={coracao} alt='coração' title='Coração' height='50px' hidth='50px' hspace='10'/>
+      </div>
+    )
+  }
+  
+  export default App;
+
+//Tentando tornar clicável (em andamento):
+  // function App(){
+//   const[exibirTitulo, setExibirTitulo]=useState(false);
+
+//   const handleClick=() => {
+//     setExibirTitulo(!exibirTitulo);
+//   };
+//   return(
+//     <div className="App">
+//       <h1>Hello, world!</h1>
+//       <img src={rindo} alt='Sorrindo' height='200px' hidth='200px'
+//       onClick={handleClick}
+//       style={{ cursor: 'pointer' }}/>
+//       {exibirTitulo && <p>Sorrindo</p>}
+
+//       <p>Clique no emoji para exibir o nome dele!</p>
+      
+//       <img src={alegre} alt='Alegre' height='50px' hidth='50px' hspace='10'
+//       onClick={handleClick}
+//       style={{ cursor: 'pointer' }}/>
+//       {exibirTitulo && <p>Alegre</p>}
+
+//       <img src={bailarina} alt='Bailarina' height='50px' hidth='50px' hspace='10'
+//       onClick={handleClick}
+//       style={{ cursor: 'pointer' }}/>
+//       {exibirTitulo && <p>Bailarina</p>}
+
+//       <img src={coracao} alt='Coração' height='50px' hidth='50px' hspace='10'
+//       onClick={handleClick}
+//       style={{ cursor: 'pointer' }}/>
+//       {exibirTitulo && <p>Coração</p>}
+//     </div>
+//   )
+// }
+
+// export default App;
+
